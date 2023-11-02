fc651042ad619e96ad32267e9d703e208096bed1
Santosh7349 <santoshhodamani0@gmail.com>
2023-11-02 17:13:59 +0530

    commentedSantosh

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import './App.css';
-import Navbar from './navbar';
-import Home from './home';
-import Favourites from './favourites';
-import Addfvrt from './addfvrt';
-import About from './about';
+import "./App.css";
+import Navbar from "./navbar";
+import Home from "./home";
+import Favourites from "./favourites";
+import Addfvrt from "./addfvrt";
+import About from "./about";
 // import State from './state';
-import Footer from './footer';
-import{ BrowserRouter, Routes , Route } from "react-router-dom"
+import Footer from "./footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
+//function
 function App() {
-  let title="Favourites Movies"
-  
+  let title = "Favourites Movies";
+
   return (
     <div className="App">
       <BrowserRouter>
-            <Navbar/>
-            <Routes>
-              <Route path='/'  element={<Home/>} />
-              <Route path='/favourites' element={<Favourites data={title}/>}/>
-              <Route path='/addfvrt' element={<Addfvrt/>}/>
-              <Route path='/about' element={<About/>}/>
-            </Routes>
-            <Footer/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites data={title} />} />
+          <Route path="/addfvrt" element={<Addfvrt />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-     
     </div>
   );
 }
